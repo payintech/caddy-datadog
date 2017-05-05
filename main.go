9e8c1f3c2d9e7c9e747676856136d906fc184b0e
Thibault Meyer <meyer.thibault@gmail.com>
2017-05-05 20:45:45 +0200

    Only init one time
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- main.go
+++ main.go
@@ -34,38 +34,116 @@ type DatadogMetrics struct {
 var glDatadogMetrics *DatadogMetrics
 
 func initializeDatadogHQ(controller *caddy.Controller) error {
-	ipAddressRegex := regexp.MustCompile(`^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}$`)
-	tagRegex := regexp.MustCompile(`^[a-zA-Z0-9]{1,25}$`)
-
 	datadog := &DatadogModule{
 		daemonAddress: "127.0.0.1:8125",
 		tags:          []string{},
 		rate:          2.0,
 	}
-	glDatadogMetrics = &DatadogMetrics{}
-	for controller.Next() {
-		for controller.NextBlock() {
-			switch controller.Val() {
-			case "statsd":
-				datadog.daemonAddress = controller.RemainingArgs()[0]
-				if !ipAddressRegex.MatchString(datadog.daemonAddress) {
-					return controller.Err("datadog: not a valid address")
-				}
-			case "tags":
-				datadog.tags = controller.RemainingArgs()
-				for idx, tag := range datadog.tags {
-					if !tagRegex.MatchString(tag) {
-						return controller.Errf("datadog: tag #%d is not valid", idx)
+	if glDatadogMetrics == nil {
+		ipAddressRegex := regexp.MustCompile(`^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}$`)
+		tagRegex := regexp.MustCompile(`^[a-zA-Z0-9]{1,25}$`)
+
+		glDatadogMetrics = &DatadogMetrics{}
+		for controller.Next() {
+			for controller.NextBlock() {
+				switch controller.Val() {
+				case "statsd":
+					datadog.daemonAddress = controller.RemainingArgs()[0]
+					if !ipAddressRegex.MatchString(datadog.daemonAddress) {
+						return controller.Err("datadog: not a valid address")
 					}
-				}
-			case "rate":
-				var err = error(nil)
-				datadog.rate, err = strconv.ParseFloat(controller.RemainingArgs()[0], 64)
-				if err != nil {
-					return controller.Err("datadog: not a valid float")
+				case "tags":
+					datadog.tags = controller.RemainingArgs()
+					for idx, tag := range datadog.tags {
+						if !tagRegex.MatchString(tag) {
+							return controller.Errf("datadog: tag #%d is not valid", idx)
+						}
+					}
+				case "rate":
+					var err = error(nil)
+					datadog.rate, err = strconv.ParseFloat(controller.RemainingArgs()[0], 64)
+					if err != nil {
+						return controller.Err("datadog: not a valid float")
+					}
 				}
 			}
 		}
+
+		daemonClient, err := statsd.New(datadog.daemonAddress)
+		if err != nil {
+			return err
+		}
+		daemonClient.SimpleEvent(
+			"Caddy - The HTTP/2 Web Server with Automatic HTTPS",
+			"Caddy server is now running...",
+		)
+		daemonClient.Namespace = "caddy."
+		daemonClient.Tags = datadog.tags
+
+		ticker := time.NewTicker(time.Second * 10)
+		quit := make(chan struct{})
+		go func() {
+			for {
+				select {
+				case <-ticker.C:
+					//fmt.Println(
+					//	glDatadogMetrics.response1xxCount,
+					//	glDatadogMetrics.response2xxCount,
+					//	glDatadogMetrics.response3xxCount,
+					//	glDatadogMetrics.response4xxCount,
+					//	glDatadogMetrics.response5xxCount,
+					//)
+					daemonClient.Gauge(
+						"core.request_per_s",
+						glDatadogMetrics.response1xxCount+
+							glDatadogMetrics.response2xxCount+
+							glDatadogMetrics.response3xxCount+
+							glDatadogMetrics.response4xxCount+
+							glDatadogMetrics.response5xxCount,
+						nil,
+						datadog.rate,
+					)
+					daemonClient.Gauge(
+						"responses.1xx",
+						glDatadogMetrics.response1xxCount,
+						nil,
+						datadog.rate,
+					)
+					daemonClient.Gauge(
+						"responses.2xx",
+						glDatadogMetrics.response2xxCount,
+						nil,
+						datadog.rate,
+					)
+					daemonClient.Gauge(
+						"responses.3xx",
+						glDatadogMetrics.response3xxCount,
+						nil,
+						datadog.rate,
+					)
+					daemonClient.Gauge(
+						"responses.4xx",
+						glDatadogMetrics.response4xxCount,
+						nil,
+						datadog.rate,
+					)
+					daemonClient.Gauge(
+						"responses.5xx",
+						glDatadogMetrics.response5xxCount,
+						nil,
+						datadog.rate,
+					)
+					glDatadogMetrics.response1xxCount = 0
+					glDatadogMetrics.response2xxCount = 0
+					glDatadogMetrics.response3xxCount = 0
+					glDatadogMetrics.response4xxCount = 0
+					glDatadogMetrics.response5xxCount = 0
+				case <-quit:
+					ticker.Stop()
+					return
+				}
+			}
+		}()
 	}
 
 	httpserver.
@@ -74,75 +152,6 @@ func initializeDatadogHQ(controller *caddy.Controller) error {
 			datadog.Next = next
 			return datadog
 		})
-
-	daemonClient, err := statsd.New(datadog.daemonAddress)
-	if err != nil {
-		return err
-	}
-	daemonClient.SimpleEvent(
-		"Caddy - The HTTP/2 Web Server with Automatic HTTPS",
-		"Caddy server is now running...",
-	)
-	daemonClient.Namespace = "caddy."
-	daemonClient.Tags = datadog.tags
-
-	ticker := time.NewTicker(time.Second * 10)
-	quit := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				daemonClient.Gauge(
-					"core.request_per_s",
-					glDatadogMetrics.response1xxCount+
-						glDatadogMetrics.response2xxCount+
-						glDatadogMetrics.response3xxCount+
-						glDatadogMetrics.response4xxCount+
-						glDatadogMetrics.response5xxCount,
-					nil,
-					datadog.rate,
-				)
-				daemonClient.Gauge(
-					"responses.1xx",
-					glDatadogMetrics.response1xxCount,
-					nil,
-					datadog.rate,
-				)
-				daemonClient.Gauge(
-					"responses.2xx",
-					glDatadogMetrics.response2xxCount,
-					nil,
-					datadog.rate,
-				)
-				daemonClient.Gauge(
-					"responses.3xx",
-					glDatadogMetrics.response3xxCount,
-					nil,
-					datadog.rate,
-				)
-				daemonClient.Gauge(
-					"responses.4xx",
-					glDatadogMetrics.response4xxCount,
-					nil,
-					datadog.rate,
-				)
-				daemonClient.Gauge(
-					"responses.5xx",
-					glDatadogMetrics.response5xxCount,
-					nil,
-					datadog.rate,
-				)
-				glDatadogMetrics.response1xxCount = 0
-				glDatadogMetrics.response2xxCount = 0
-				glDatadogMetrics.response3xxCount = 0
-				glDatadogMetrics.response4xxCount = 0
-				glDatadogMetrics.response5xxCount = 0
-			case <-quit:
-				ticker.Stop()
-				return
-			}
-		}
-	}()
 
 	return nil
 }
